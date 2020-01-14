f15d1e688bcd37a5fadc49693d9f281c7dd904a8
Matthew Kenworthy <kenworthy@strw.leidenuniv.nl>
2020-01-14 09:52:41 +0100

    added CDS scripts

---
+++ CDS/README.md
@@ -0,0 +1,14 @@
+# CDS upload
+
+Setting up the image files for submission to the CDS archive.
+
+## NACO image
+
+Copy over the image and add FITS keywords and a WCS header.
+
+	python add_keywords_naco.py
+
+## Copy over the ALMA image
+
+	cp ../alma_analysis/uid___A001_X87c_X409._1SWASP_J140747.93-394542.6__sci.spw17_19_21_23.cont.I.pbcor.fits ALMA1407.FIT
+
--- CDS/add_keywords_naco.py
+++ CDS/add_keywords_naco.py
@@ -1,19 +1,21 @@
 from astropy.io import fits
 
-# M Kenworthy making the NACO J1407 data contain FITS keywords
+# M Kenworthy adding FITS keywords and a WCS to a 2D image for CDS upload
+
+fileout = 'naco1407.fit'
 
 with fits.open('../naco_analysis/J1407_NACO_result.fits') as hdu1:
 
     hdu1.info()
 
     hdr = hdu1[0].header
-    #print(repr(hdr))
     # http://simbad.u-strasbg.fr/simbad/sim-basic?Ident=V1400+Cen&submit=SIMBAD+search
     # ICRS coord. (ep=J2000) :	14 07 47.9296051120 -39 45 42.766332988
 
-
     from astropy import units as u
     from astropy.coordinates import Angle
+
+    # input cooedinates
     ra  = Angle('14h07m47.92s')
     dec = Angle('-39d45m42.7s')
     print(ra.degree)
@@ -27,15 +29,13 @@ with fits.open('../naco_analysis/J1407_NACO_result.fits') as hdu1:
     hdr['RA']       = ra.degree
     hdr['DEC']      = dec.degree
     hdr['EQUINOX']  = 2000.0
-#    hdr['RADECSYS'] = 'FK5'
     hdr['EXPTIME']  = 5220
     hdr['FOV']      = 0.001
     hdr['FILTER']   = 'L_prime'
     hdr['WAVELMAX']  = 4105
     hdr['WAVELMIN']  = 3495
     hdr['WLENUNIT'] = 'nm'
-#    hdr['NAXIS']    = 2
-#    hdr.remove('NAXIS3')
+    hdr['NAXIS']    = 2
 
     # now write in WCS....
     import numpy as np
@@ -43,20 +43,17 @@ with fits.open('../naco_analysis/J1407_NACO_result.fits') as hdu1:
 
     w = wcs.WCS(naxis=2)
 
-    # Set up an "Airy's zenithal" projection
-    # Vector properties may be set with Python lists, or Numpy arrays
     w.wcs.crpix = [51.0, 51.0] # location of star in pixels
     pscale = 0.02719 # pixel scale (arcsec/pixel)
     w.wcs.cdelt = np.array([-pscale/3600., pscale/3600.]) # NOTE the minus for correct RA!
     w.wcs.crval = [ra.degree, dec.degree]
     w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
-#    w.wcs.set_pv([(2, 1, 45.0)])
 
     wcsheader = w.to_header()
     hdr.extend(wcsheader)
     print(wcsheader)
 
-    hdu1.writeto('naco1407.fit')
+    hdu1.writeto(fileout)
 
 
 from matplotlib import pyplot as plt
@@ -64,8 +61,7 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.utils.data import get_pkg_data_filename
 
-filename = 'naco1407.fit'
-hdu = fits.open(filename)[0]
+hdu = fits.open(fileout)[0]
 wcs = WCS(hdu.header, naxis=2)
 
 fig = plt.figure()
